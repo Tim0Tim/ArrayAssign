a61831ba7106e104c26318f91055842475218466
Tim0Tim <graden.bishop2995@omail.otech.edu>
2022-04-29 14:35:38 -0600

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+//first create variables to place in array
+//    then create comparision to prevent any repetitions
+//    then create array
+//    create write output
