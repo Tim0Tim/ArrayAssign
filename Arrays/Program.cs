2f7027419079c38e3512b7d150bc60bd8068e479
Tim0Tim <graden.bishop2995@omail.otech.edu>
2022-04-29 14:49:55 -0600

    rough draft first half

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -4,9 +4,35 @@ namespace Arrays
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Random r1 = new Random();
+            int r1Int = r1.Next(10 ,100);
+            Random r2 = new Random();
+            int r2Int = r2.Next(10 ,100);
+            Random r3 = new Random();
+            int r3Int = r3.Next(10 ,100);
+            Random r4 = new Random();
+            int r4Int = r4.Next(10 ,100);
+            Random r5 = new Random();
+            int r5Int = r5.Next(10 ,100);
+
+            if (r1 == r2)
+            {
+                r2 = new Random();
+            }
+            if (r3 == r1 || r3 == r2)
+            {
+                r3 = new Random();
+            }
+            if (r4 == r1 || r4 == r2 || r4 == r3)
+            {
+                r4 = new Random();
+            }
+            if (r5 == r1 || r5 == r2 || r5 == r3 || r5 == r4)
+            {
+                r5 = new Random();
+            }
         }
     }
 }
